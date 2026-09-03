2c2c7009659a18240cb34e557504b48d290947b7
Sachinnn22 <sachinduchirau2@gmail.com>
2026-09-03 20:16:38 +0530

    changed  service files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,24 +8,24 @@ module.exports = {
         },
         {
             name: "user-service",
-            script: "java",
-            args: "-jar ./user-service/target/user-service-0.0.1-SNAPSHOT.jar",
+            script: "/home/thenujarathnayaka9/.sdkman/candidates/java/current/bin/java",
+            args: "-Xms128m -Xmx256m -jar ./user-service/target/user-service-0.0.1-SNAPSHOT.jar",
             log_file: "./logs/user-service.log",
-            instances: 2,
+            instances: 1,
         },
         {
             name: "salon-service",
-            script: "java",
-            args: "-jar ./salon-service/target/salon-service-0.0.1-SNAPSHOT.jar",
+            script: "/home/thenujarathnayaka9/.sdkman/candidates/java/current/bin/java",
+            args: "-Xms128m -Xmx256m -jar ./salon-service/target/salon-service-0.0.1-SNAPSHOT.jar",
             log_file: "./logs/salon-service.log",
-            instances: 2,
+            instances: 1,
         },
         {
             name: "appoinment-service",
-            script: "java",
-            args: "-jar ./appoinment-service/target/appointment-service-0.0.1-SNAPSHOT.jar",
+            script: "/home/thenujarathnayaka9/.sdkman/candidates/java/current/bin/java",
+            args: "-Xms128m -Xmx256m -jar ./appoinment-service/target/appointment-service-0.0.1-SNAPSHOT.jar",
             log_file: "./logs/appointment-service.log",
-            instances: 2,
+            instances: 1,
         }
     ]
 };
